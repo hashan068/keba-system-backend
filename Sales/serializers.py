c19cd10286eb882882f5667dfd1cf60518033a3c
Hashan Ruchira <hashanruchira98@gmail.com>
2024-04-24 17:51:20 +0530

    Sales order Serializer Updated

--- Sales/serializers.py
+++ Sales/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from decimal import Decimal
+
 from .models import Customer, Product, SalesOrder, SalesOrderItem
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -6,18 +8,16 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = Customer
         fields = '__all__'
 
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
-
 class SalesOrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = SalesOrderItem
         fields = ['product', 'quantity', 'price']
-
+        
 
 class SalesOrderSerializer(serializers.ModelSerializer):
     order_items = SalesOrderItemSerializer(many=True)
@@ -28,7 +28,14 @@ class SalesOrderSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         order_items_data = validated_data.pop('order_items')
+
+        total_amount = sum(Decimal(item['price']) * item['quantity'] for item in order_items_data)
+        validated_data['total_amount'] = total_amount
+
         sales_order = SalesOrder.objects.create(**validated_data)
+
         for order_item_data in order_items_data:
             SalesOrderItem.objects.create(order=sales_order, **order_item_data)
+
         return sales_order
+
