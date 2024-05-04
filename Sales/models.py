d8eea153968d8ca505e7c3e29cd0888a064727ab
Hashan Ruchira <hashanruchira98@gmail.com>
2024-05-04 12:08:47 +0530

    Add photo field to Product model in Sales

--- Sales/models.py
+++ Sales/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    # photo = models.ImageField(upload_to="cars")
 
     def __str__(self):
         return self.name
